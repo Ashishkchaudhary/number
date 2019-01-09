50d64d57eacfe8de24270bb7f8e35b2ad15ea751
ashishkchaudhary <ashish.93kumark@gmail.com>
2019-01-09 12:54:08 +0530

    hi

---
+++ src/DiamondRingPage.java
@@ -0,0 +1,31 @@
+
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DiamondRingPage {
+
+	
+	
+	
+	
+	
+	public DiamondRingPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	
+	public void getPriceList()
+	{
+		
+//		int size = Defaultpricelst.size();
+	//		System.out.println(size);
+	}
+}
